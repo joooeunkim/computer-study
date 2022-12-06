40f4b5d0d0a7c2a80f185b5fb37fd39bc0a3842e
hjay1029 <hjay1029@naver.com>
2022-12-06 23:17:07 +0900

    [BOJ]/2798/파이썬

---
+++ hjk/[BOJ]_2798.py
@@ -0,0 +1,17 @@
+//풀이 시간 : 15분
+//문제 핵심 내용 : vor문을 여러번 돌려서 푸는 문제
+//회고 : 쉬운 문제들을 좀 많이 풀면서 다시 알고리즘 감을 익힌 후에 다음 단계로 넘어가려고 한다. 
+
+
+N,M = map(int,input().split())
+lst = list(map(int, input().split()))
+result = 0
+a = []
+
+for i in range(N):
+    for j in range(i+1, N):
+        for k in range(j+1, N):
+            if lst[i] + lst[j] + lst[k] <= M:
+                a.append(lst[i] + lst[j] + lst[k])
+
+print(max(a))
