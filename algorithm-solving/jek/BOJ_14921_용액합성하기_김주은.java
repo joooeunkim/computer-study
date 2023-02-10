2eb32589fc46adc85aed7b51d38c80a56ea06400
JooEunKim <44815092+joooeunkim@users.noreply.github.com>
2023-02-10 16:01:06 +0900

    [BOJ] 14921_용액합성하기_java

---
+++ algorithm-solving/jek/BOJ_14921_용액합성하기_김주은.java
@@ -0,0 +1,46 @@
+//풀이 시간 : 14분
+//풀이 핵심 내용 : 투포인터를 사용하기
+//회고 : Scanner 부분에서 헤메었다.
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_BJ_14921_용액합성하기_김주은 {
+
+	public static void main(String[] args) throws Exception{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] liquid = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			liquid[i]=Integer.parseInt(st.nextToken());
+		}
+		
+		int start=0;
+		int end=N-1;
+		
+		int min = 100000000;
+		
+		while(start<end) {
+			int val = liquid[start]+liquid[end];
+			if(Math.abs(val)<Math.abs(min)) {
+				min = val;
+			}
+			if(val<0) {
+				start++;
+			}else if(val>0) {
+				end--;
+			}else {
+				break;
+			}
+		}
+		
+		System.out.println(min);
+		
+	}
+
+}
