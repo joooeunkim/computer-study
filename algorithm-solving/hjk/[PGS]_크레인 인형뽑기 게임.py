03d417211018b3d0e820cf1307936901b48d9753
hjay1029 <hjay1029@naver.com>
2023-04-07 23:56:37 +0900

    [PGS]/크레인/python

--- algorithm-solving/hjk/[PGS]_크레인 인형뽑기 게임.py
+++ algorithm-solving/hjk/[PGS]_크레인 인형뽑기 게임.py
@@ -1,20 +1,20 @@
-//풀이 시간 : 20분
+//풀이 시간 : 30분
 //문제 핵심 내용 : 배열판 문제
-//회고 : 반복문을 여러번 활용해 풀 수 있다.
+//회고 : 반복문을 여러 번 사용해서 풀이
 
 def solution(board, moves):
     answer = 0
-    res = []
+    result = []
 
     for i in moves:
         for j in range(len(board)):
             if board[j][i-1] != 0:
-                res.append(board[j][i-1])
+                result.append(board[j][i-1])
                 board[i][j-1] = 0
                 break
-        if len(res) >= 2:
-            for k in range(len(res)-1):
-                if res[k] == res[k+1]:
+        if len(result) >= 2:
+            for k in range(len(result)-1):
+                if result[k] == result[k+1]:
                     answer += 2
-                    res = res[:k]
+                    result = result[:k]
     return answer
