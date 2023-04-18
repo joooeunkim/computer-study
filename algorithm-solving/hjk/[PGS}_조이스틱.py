5b2382b9bf58276bf37e63c2ad542781a77dc2fb
hjay1029 <hjay1029@naver.com>
2023-04-18 22:36:07 +0900

    [PGS]/조이스틱/python

--- algorithm-solving/hjk/[PGS}_조이스틱.py
+++ algorithm-solving/hjk/[PGS}_조이스틱.py
@@ -4,14 +4,14 @@
 
 
 def solution(name):
-    answer = 0
-    min_move = len(name) - 1
+    ans = 0
+    move = len(name) - 1
     for i, char in enumerate(name):
-        answer += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
+        ans += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
 
         next = i + 1
         while next < len(name) and name[next] == 'A':
             next += 1
-        min_move = min([min_move, 2 * i + len(name) - next, i + 2 * (len(name) - next)])
-    answer += min_move
-    return answer
+        move = min([move, 2 * i + len(name) - next, i + 2 * (len(name) - next)])
+    ans += move
+    return ans
