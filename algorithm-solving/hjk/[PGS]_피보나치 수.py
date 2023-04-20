6e138c6468c7d675f069e115904de1b955e2a097
hjay1029 <hjay1029@naver.com>
2023-04-20 22:51:57 +0900

    [PGS]/피보나치수/python

--- algorithm-solving/hjk/[PGS]_피보나치 수.py
+++ algorithm-solving/hjk/[PGS]_피보나치 수.py
@@ -3,10 +3,10 @@
 //회고 : 피보나치 수열 문제 푸는 법 기억하기
 def solution(n):
     answer = 0자
-    f = [1, 1]
+    fibo = [1, 1]
 
     for i in range(n):
-        f.append(f[i] + f[i + 1])
+        fibo.append(fibo[i] + fibo[i + 1])
 
-    answer = f[n - 1] % 1234567
+    answer = fibo[n - 1] % 1234567
     return answer
