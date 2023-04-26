989d4906ff3232c6b91adcd8089a0773fb1cc2b5
hjay1029 <hjay1029@naver.com>
2023-04-26 22:44:51 +0900

    [PGS]/카펫/python

--- algorithm-solving/hjk/[PGS]_카펫.py
+++ algorithm-solving/hjk/[PGS]_카펫.py
@@ -1,18 +1,20 @@
-//풀이 시간 : 40분
+//풀이 시간 : 30분
 //문제 핵심 내용 : 완전탐색 문제
 //회고 : 노란색과 브라운색의 갯수 차이를 생각해내면 쉽게 풀리는 문제
 
-
 def solution(brown, yellow):
-    answer = []
-    total = brown + yellow
     yellow_x = 0
     yellow_y = 0
+    ans = []
 
-    for i in range(1, yellow + 1):
+    for i in range(1, yellow+1):
         if yellow % i == 0:
-            if i >= yellow // i:
-                yellow_x = i
-                yellow_y = yellow // i
-                if (yellow_x * 2 + yellow_y * 2 + 4) == brown:
-                    return [yellow_x + 2, yellow_y + 2]
+            yellow_y = i
+            yellow_x = yellow // i
+
+        if yellow_x * 2 + yellow_y * 2 + 4 == brown:
+            ans.append(yellow_x+2)
+            ans.append(yellow_y+2)
+            break
+
+    return ans
